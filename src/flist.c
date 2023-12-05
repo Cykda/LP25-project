5fca96c70ba23a5b5a53a2db002c0058930f085e
Gabriel Chaillet <gabriel.chaillet@orange.fr>
2023-12-05 17:28:37 +0100

    FS linked list made

---
+++ src/flist.c
@@ -0,0 +1,146 @@
+#include "flist.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/*!
+ * @brief clear_files_list clears a files list
+ * @param list is a pointer to the list to be cleared
+ * This function is provided, you don't need to implement nor modify it
+ */
+void clear_files_list(files_list_t *list) {
+    while (list->head) {
+        files_list_entry_t *tmp = list->head;
+        list->head = tmp->next;
+        free(tmp);
+    }
+}
+
+/*!
+ *  @brief add_file_entry adds a new file to the files list.
+ *  It adds the file in an ordered manner (strcmp) and fills its properties
+ *  by calling stat on the file.
+ *  Il the file already exists, it does nothing and returns 0
+ *  @param list the list to add the file entry into
+ *  @param file_path the full path (from the root of the considered tree) of the file
+ *  @return a pointer to the added element if success, NULL else (out of memory)
+ */
+files_list_entry_t *add_file_entry(files_list_t *list, char *file_path) {
+    
+    
+    files_list_entry_t *new_file_entry = malloc(sizeof(files_list_entry_t));
+    
+    if (new_file_entry == NULL) {return NULL;}
+    
+
+    files_list_entry_t *tmp = list->head;
+    strncpy(new_file_entry->path_and_name, file_path, sizeof(new_file_entry->path_and_name));
+
+    while (tmp->next != NULL)
+    {
+        if (strcmp(tmp->path_and_name, file_path) == 0)
+        {
+            return new_file_entry;
+        }
+        else 
+        {
+            if (strcmp(tmp->path_and_name, file_path) < 0 )
+            {
+                new_file_entry->prev = tmp;
+                new_file_entry->next = tmp->next;
+                tmp->next = new_file_entry;
+                return new_file_entry;
+            }
+            else 
+            {
+                tmp = tmp->next;
+            }
+        }
+    }
+    if (tmp->next == NULL) 
+    {
+        tmp->next = new_file_entry;
+        new_file_entry->prev = tmp;
+        new_file_entry->next = NULL;
+        return new_file_entry;
+    }
+
+}
+
+/*!
+ * @brief add_entry_to_tail adds an entry directly to the tail of the list
+ * It supposes that the entries are provided already ordered, e.g. when a lister process sends its list's
+ * elements to the main process.
+ * @param list is a pointer to the list to which to add the element
+ * @param entry is a pointer to the entry to add. The list becomes owner of the entry.
+ * @return 0 in case of success, -1 else
+ */
+int add_entry_to_tail(files_list_t *list, files_list_entry_t *entry) {
+    if (entry == NULL) {return -1;}
+
+    entry->next = NULL;
+    entry->prev = list->tail;
+    if (list->head == NULL)
+    {
+        list->head = entry;
+    } 
+    else
+    {
+        list->tail->next = entry;
+    }
+    list->tail = entry;
+    return 0;
+}
+
+/*!
+ *  @brief find_entry_by_name looks up for a file in a list
+ *  The function uses the ordering of the entries to interrupt its search
+ *  @param list the list to look into
+ *  @param file_path the full path of the file to look for
+ *  @param start_of_src the position of the name of the file in the source directory (removing the source path)
+ *  @param start_of_dest the position of the name of the file in the destination dir (removing the dest path)
+ *  @return a pointer to the element found, NULL if none were found.
+ */
+files_list_entry_t *find_entry_by_name(files_list_t *list, char *file_path, size_t start_of_src, size_t start_of_dest) {
+
+    files_list_entry_t* cursor = list->head;
+    while (cursor != NULL)
+    {
+        if (strcmp(cursor->path_and_name + start_of_src, file_path + start_of_dest) == 0)
+        {
+            return cursor;
+        }
+        cursor = cursor->next;
+    }
+    return NULL;
+
+}
+
+/*!
+ * @brief display_files_list displays a files list
+ * @param list is the pointer to the list to be displayed
+ * This function is already provided complete.
+ */
+void display_files_list(files_list_t *list) {
+    if (!list)
+        return;
+    
+    for (files_list_entry_t *cursor=list->head; cursor!=NULL; cursor=cursor->next) {
+        printf("%s\n", cursor->path_and_name);
+    }
+}
+
+/*!
+ * @brief display_files_list_reversed displays a files list from the end to the beginning
+ * @param list is the pointer to the list to be displayed
+ * This function is already provided complete.
+ */
+void display_files_list_reversed(files_list_t *list) {
+    if (!list)
+        return;
+    
+    for (files_list_entry_t *cursor=list->tail; cursor!=NULL; cursor=cursor->prev) {
+        printf("%s\n", cursor->path_and_name);
+    }
+}
--- src/main.c
+++ src/main.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "flist.h"
 
 
+void clear_flt(files_list_t *flt) // files_list_t *list
+{
+    printf("CLEAR START\n");
+    
+    if(flt->head == NULL)
+    {
+        printf("NULL HEAD\n");
+        return;
+        
+    }
+    while (flt->head) {
+        files_list_entry_t *tmp = flt->head;
+        flt->head = tmp->next;
+        free(tmp);
+    }
+    
+}
 int main(int argc, char** argv)
 {
     
+    printf("START\n");
+    files_list_t *flt = malloc(sizeof(file_type_t));
+    flt->head = malloc(sizeof(files_list_entry_t));
+    flt->tail = NULL;
     
-    printf("Hello World\n");
     
     
+    
+    if(flt == NULL) {return EXIT_FAILURE;}
+    
+    printf("flt variable allocated successfully\n");
+    
+    
+    add_file_entry(flt, "C:/");
+    add_file_entry(flt, "C:/Windows");
+    add_file_entry(flt, "C:/Windows/Users");
+
+    
+    
+    display_files_list(flt);
+    clear_files_list(flt);
     return 0;
 }
